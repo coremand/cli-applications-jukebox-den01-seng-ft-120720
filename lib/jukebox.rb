229601a3d6c113b2e2b69d94aa49ae0142c4b097
Obinna Nwabia <valinevite@gmail.com>
2020-12-03 15:10:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,9 +39,9 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
+  
   if list_songs(songs)[user_input.to_i] 
     puts "Playing #{list_songs(songs)[user_input.to_i]}"
-    elsif 
   else
     puts "Invalid input, please try again"
   end
