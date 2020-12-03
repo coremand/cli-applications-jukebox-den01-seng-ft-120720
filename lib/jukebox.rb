7a069659fdd684e8c1ff3c117b59fd57a1844f8f
Obinna Nwabia <valinevite@gmail.com>
2020-12-03 16:16:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,7 +55,17 @@ end
 
 
 def run
-  puts ""
+  puts "Please enter a command:"
+  user_input = gets.strip
+  if user_input == "list"
+    list(songs)
+    elsif user_input == "play"
+      play(songs)
+    elsif user_input == "help"
+    help
+  else
+    exit_jukebox
+  end
 end
 
 
